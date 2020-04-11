1f221826d8bf4a6193701edb1d27f4619090bef6
Kelly M Collins <90kcol@gmail.com>
2020-04-11 16:25:59 -0400

    Added side navigation bar to shop page.  Need to create links that filter the content of the page

--- src/Directory/Shop.js
+++ src/Directory/Shop.js
@@ -4,45 +4,57 @@ import './Shop.css'
 class Shop extends Component {
     render () {
         return (
-            <div className='container shopDiv'>
-                <div class="row row-cols-1 row-cols-md-3">
-                    <div class="col mb-4">
-                        <div class="card h-100 w-100">
-                        <img className='shopImage' src={placeholder}  class="card-img-top" alt="placeholder"/>
+            <div className='shopDiv'>
+                <div className='shopSideNav'>
+                    <ul>
+                        <li>Tops</li>
+                        <li>Bottoms</li>
+                        <li>Blazers</li>
+                        <li>Outerwear</li>
+                        <li>Sequins</li>
+                        <li>Accessories</li>
+                    </ul>
+                </div>
+                <div className='container shopContent'>
+                    <div class="row row-cols-1 row-cols-md-3">
+                        <div class="col mb-4">
+                            <div class="card h-100 w-100">
+                            <img className='shopImage' src={placeholder}  class="card-img-top" alt="placeholder"/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Example</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card h-100 w-100">
+                            <img className='shopImage' src={placeholder}  class="card-img-top" alt="placeholder"/>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Example</p>
                             </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col mb-4">
-                        <div class="card h-100 w-100">
-                        <img className='shopImage' src={placeholder}  class="card-img-top" alt="placeholder"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Example</p>
+                        <div class="col mb-4">
+                            <div class="card h-100 w-100" >
+                            <img className='shopImage' src={placeholder}  class="card-img-top" alt="placeholder"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Example.</p>
+                            </div>
+                            </div>
                         </div>
-                        </div>
-                    </div>
-                    <div class="col mb-4">
-                        <div class="card h-100 w-100" >
-                        <img className='shopImage' src={placeholder}  class="card-img-top" alt="placeholder"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Example.</p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col mb-4">
-                        <div class="card h-100 w-100">
-                        <img className='shopImage' src={placeholder}  class="card-img-top" alt="placeholder"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Example</p>
+                        <div class="col mb-4">
+                            <div class="card h-100 w-100">
+                            <img className='shopImage' src={placeholder}  class="card-img-top" alt="placeholder"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Example</p>
+                            </div>
+                            </div>
                         </div>
                         </div>
-                    </div>
-                    </div>
+                </div>
             </div>
         )
     }
