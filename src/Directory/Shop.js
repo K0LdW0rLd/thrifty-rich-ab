6bf71f816c4e44821a0c95a755e1742eef050fd5
Kelly M Collins <90kcol@gmail.com>
2020-04-19 16:52:07 -0400

    Worked on shop tap. work on filter when backend is linked.

--- src/Directory/Shop.js
+++ src/Directory/Shop.js
@@ -2,11 +2,26 @@ import React, {Component} from "react";
 import placeholder from './images/placeholder.jpg';
 import './Shop.css';
 class Shop extends Component {
-    render () {
+    // Constructor is a reminder of code
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       isOpen: false,
+    //       isClosed: true,
+    //       };
+    //   }
+  
+    //   toggleMenu() {
+    //     this.setState({
+    //         isOpen: !this.state.isOpen,
+    //         isClosed: !this.state.isClosed
+    //       });
+    //   }
+      render () {
         return (
             <div className='shopDiv'>
                 <div className='shopSideNav'>
-                    {/* Add code to make the a tags filter the backend results.  Waiting on backend to be built first */}
+                    {/* Add code to make the a tags filter the backend results. Going to put the results into an array and filter that way.  Waiting on backend to be built first */}
                     <p><a href='#'>Tops</a></p>
                     <p><a href='#'>Bottoms</a></p>
                     <p><a href='#'>Blazers</a></p>
